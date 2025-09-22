343b6ed45180a02ca785bc8a72425bb7cfc6935f
IAmBatJew <aamigarr@gmail.com>
2025-09-21 20:26:44 -0400

    idk

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './css/App.css';
 import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Router basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Home />} />
           {/* <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} /> */}
         </Routes>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
